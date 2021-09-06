ba3419351e908bf8c6e723913c6f0161ba72a8a3
janner <janner@mit.edu>
2021-09-05 23:11:33 -0700

    using default doodad parameters

--- azure/launch_plan.py
+++ azure/launch_plan.py
@@ -4,9 +4,15 @@ import pdb
 from doodad.wrappers.easy_launch import sweep_function, save_doodad_config
 
 codepath = '/home/code'
-script = 'scripts/train.py'
+script = 'scripts/plan.py'
 
 def remote_fn(doodad_config, variant):
+    ## get suffix range to allow running multiple trials per job
+    n_suffices = variant['n_suffices']
+    suffix_start = variant['suffix_start']
+    del variant['n_suffices']
+    del variant['suffix_start']
+
     kwarg_string = ' '.join([
         f'--{k} {v}' for k, v in variant.items()
     ])
@@ -15,19 +21,23 @@ def remote_fn(doodad_config, variant):
     d4rl_path = os.path.join(doodad_config.output_directory, 'datasets/')
     os.system(f'ls -a {codepath}')
     os.system(f'mv {codepath}/git {codepath}/.git')
-    os.system(
-        f'''export PYTHONPATH=$PYTHONPATH:{codepath} && '''
-        f'''export CUDA_VISIBLE_DEVICES=0 && '''
-        f'''export D4RL_DATASET_DIR={d4rl_path} && '''
-        f'''python {os.path.join(codepath, script)} '''
-        f'''{kwarg_string}'''
 
-    )
+    for suffix in range(suffix_start, suffix_start + n_suffices):
+        os.system(
+            f'''export PYTHONPATH=$PYTHONPATH:{codepath} && '''
+            f'''export CUDA_VISIBLE_DEVICES=0 && '''
+            f'''export D4RL_DATASET_DIR={d4rl_path} && '''
+            f'''python {os.path.join(codepath, script)} '''
+            f'''--suffix {suffix} '''
+            f'''{kwarg_string}'''
+
+        )
+
     save_doodad_config(doodad_config)
 
 if __name__ == "__main__":
 
-    environments = ['ant']
+    environments = ['halfcheetah']
     buffers = ['medium-expert-v2', 'medium-v2', 'medium-replay-v2', 'random-v2']
     datasets = [f'{env}-{buf}' for env in environments for buf in buffers]
 
@@ -39,7 +49,10 @@ if __name__ == "__main__":
 
     default_params = {
         'logbase': os.path.join('/doodad_tmp', azure_logpath, 'logs'),
-        'exp_name': 'gpt/azure',
+        'prefix': 'plans/azure/',
+        'verbose': False,
+        'suffix_start': 40,
+        'n_suffices': 3,
     }
 
     sweep_function(
@@ -48,7 +61,7 @@ if __name__ == "__main__":
         default_params=default_params,
         config_path=os.path.abspath('azure/config.py'),
         log_path=azure_logpath,
-        gpu_model='nvidia-tesla-v100',
+        gpu_model='nvidia-tesla-t4',
         filter_dir=['logs', 'bin'],
         use_gpu=True,
     )
