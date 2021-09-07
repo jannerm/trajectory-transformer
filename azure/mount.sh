a300dafbdd02af16969b8f19783db0e1354ca6dc
janner <janner@mit.edu>
2021-09-07 11:27:46 -0700

    azure mounting

--- azure/mount.sh
+++ azure/mount.sh
@@ -1,2 +1,2 @@
-mkdir mount
-blobfuse mount --tmp-path=/tmp --config-file=./azure/fuse.cfg
+mkdir ~/azure_mount
+blobfuse ~/azure_mount --tmp-path=/tmp --config-file=./azure/fuse.cfg
--- azure/umount.sh
+++
@@ -1,2 +0,0 @@
-sudo umount -f mount
-rm -r mount
